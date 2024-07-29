87c06117cad4306c827c89cfd93c7edff3f66ef4
Aman Gupta <khwaabknight@gmail.com>
2024-07-29 11:38:35 +0530

    added methods to backend

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ cloudinaryConnect();
 
 app.use(cors({
   origin: '*',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }))
 
 app.use(express.json())
